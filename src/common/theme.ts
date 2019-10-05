f78a47f5f732dde789f90a98852cdf1c917a1c98
Andy Kan <andy.kmc@hotmail.com>
2019-10-05 23:10:05 +0800

    Support styled-components

---
+++ src/common/theme.ts
@@ -0,0 +1,10 @@
+import styled, { DefaultTheme, createGlobalStyle } from 'styled-components';
+
+const readerTheme: DefaultTheme = {
+  colors: {
+    main: 'cyan',
+    secondary: 'magenta',
+  },
+};
+
+export { readerTheme };
